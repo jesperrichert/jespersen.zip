c367113f453376ff72ca557ebb0f5b48bdc9ce52
xyzjesper <dm@xyzjesper>
2025-11-24 22:30:18 +0100

    More redisign...

--- src/components/tabs/Home.svelte
+++ src/components/tabs/Home.svelte
@@ -1,6 +1,38 @@
 <div class="flex items-center justify-center">
   <div class="mt-96"></div>
   <div>
-    <h1>👋 Hey and welcome!</h1>
+    <div class="inline-flex justify-between items-center mt-40">
+      <span class="animate-bounce mr-2"> 👋 </span>
+      <h1 class="text-2xl font-semibold">Hey and welcome!</h1>
+    </div>
+    <span> While writing this i think hwo i can optimize this!</span>
+    <p>
+      Any ways.... You are welcome. I do some stuff in Minecraft, Discord and
+      software development.
+      <br />
+      I'm a german Full Stack Developer. Speak in various languages.
+      <br />
+      And develop Software for the rest of us.
+      <br />
+      Read more about my projects at
+      <a
+        class="underline text-green-400"
+        href="https://github.com/xyzjesper?tab=repositories">Github</a
+      >
+      or see
+      <a
+        class="underline text-pink-400"
+        href="https://github.com/xyzjesper#-projekte--vorschau">teams</a
+      >
+      i'm in. And see the definded
+      <a
+        class="underline text-blue-400"
+        href="https://github.com/xyzjesper#tech-stack">tech stack</a
+      ><br />
+      also at my
+      <a class="underline text-lime-400" href="https://github.com/xyzjesper"
+        >read.me</a
+      >...
+    </p>
   </div>
 </div>
