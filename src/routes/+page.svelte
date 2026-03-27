2809bb22c4e85b9398a3223644f02810d963c1f7
xyzjesper <dm@xyzjesper>
2026-03-27 21:59:27 +0100

    updated navbar to sidebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 
     let navBar: NavBar;
     let navTab: string;
+    let mobileOpen = false;
     onMount(async () => {
         console.log("[onMount] Loaded NavBar, Tabs")
         navTab = window.location.href.split("?tab=")[1] ? window.location.href.split("?tab=")[1] : "home";
@@ -20,25 +21,42 @@
 </script>
 
 <div>
-    <nav class="relative  animate-glowe p-2 shadow-none rounded-b-[100px] bg-linear-to-t from-zinc-300 to-gray-500">
-        <div>
-            <div class="space-y-1 px-5 pt-2 pb-3 mt-2 rounded-2xl">
+
+    <button
+            aria-label="mobile-sidebar"
+            on:mouseleave={() => mobileOpen = false}
+            on:click={() => mobileOpen = true}
+            data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar"
+            type="button"
+            class="outline-0 ring-0 text-heading bg-transparent border-0 hover:bg-neutral-secondary-medium font-medium leading-5 rounded-base ms-3 mt-3 text-sm p-2 focus:outline-none inline-flex sm:hidden">
+        <svg class="{mobileOpen ? `text-transparent` : ``} w-6 h-6" aria-hidden="true"
+             xmlns="http://www.w3.org/2000/svg"
+             width="24" height="24" fill="none"
+             viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h10"/>
+        </svg>
+    </button>
+
+    <aside id="default-sidebar"
+           class="fixed top-0 left-0 z-40 w-64 h-full transition-transform sm:translate-x-0 {mobileOpen ? ``: `-translate-x-full`}"
+           aria-label="Sidebar">
+
+        <div class="h-full px-3 py-4 bg-neutral-primary-soft">
+            <ul class="space-y-2 font-medium">
                 {#each navBar as navItem}
-                    <button
-                            data-state={navItem.name === navTab ? "active" : "inactive"}
-                            class="{navItem.name === navTab
+                    <li>
+                        <button on:click={() => (navTab = navItem.name)}
+                                data-state={navItem.name === navTab ? "active" : "inactive"}
+                                class="{navItem.name === navTab
               ? ' bg-fuchsia-500/20'
-              : 'bg-fuchsia-200/60'}  ml-5 md:mb-10 w-15 h-10 cursor-pointer ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium capitalize transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-                            on:click={() => (navTab = navItem.name)}
-                    >
-                        <img src={navItem.icon} alt="icon"/>
-                    </button>
+              : 'bg-fuchsia-200/60'} cursor-pointer flex items-center justify-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group rounded-2xl w-12 h-10 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">
+                            <img src={navItem.icon} alt="icon"/>
+                        </button>
+                    </li>
                 {/each}
-            </div>
-
+            </ul>
         </div>
-
-    </nav>
+    </aside>
 
     {#if navTab === "home"}
         <section>
